da04cb1fc324a04068744501e06691b1234a47b5
yura <yoorachoi8937@gmail.com>
2022-01-17 20:33:49 +0900

    Read image when called

--- dataset/cubox.py
+++ dataset/cubox.py
@@ -16,29 +16,36 @@ class CUBOXdataset(Dataset):
         self.classes = sorted(os.listdir(self.dataset_path))
         self.transform = transform
         self.imgs = []
+        self.img_names = []
         self.labels = []
 
         for cls_id, cls in enumerate(self.classes):
             cls_path = os.path.join(self.dataset_path, cls)
             for filename in os.listdir(cls_path):
-                img = Image.open(os.path.join(cls_path, filename))
-                self.imgs.append(np.array(img))
-                img.close()
+                # img = Image.open(os.path.join(cls_path, filename))
+                self.img_names.append(os.path.join(cls_path, filename))
+                # self.imgs.append(np.array(img))
+                # img.close()
                 self.labels.append(cls_id)
 
         self.num_classes = len(self.classes)
 
     def __len__(self):
-        return len(self.imgs)
+        # return len(self.imgs)
+        return len(self.img_names)
 
     def __getitem__(self, idx):
         item_dict = dict()
-        img = self.imgs[idx]
+        # img = self.imgs[idx]
+        img_name = self.img_names[idx]
         if self.transform is not None:
+            img = Image.open(img_name)
+            img = np.array(img)
             img = self.transform(img)
         item_dict['img'] = img
         label = self.labels[idx]
         item_dict['gt'] = label
+        item_dict['img_name'] = img_name
         return item_dict
 
 
