3d0e36bd75c916233b6a8773d34a65c9c4a167ca
Yuuraa <yoorachoi8937@gmail.com>
2022-02-04 17:48:10 +0900

    Recover code

--- utils/save.py
+++ utils/save.py
@@ -5,6 +5,4 @@ def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
     """
     Save the training model
     """
-    if is_best:
-        filename = filename.split(".")[0] + "_best." + filename.split(".")[1]
     torch.save(state, filename)
