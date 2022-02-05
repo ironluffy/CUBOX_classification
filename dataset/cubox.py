5ba1c44e94c46659bb6658446c2f3940b9f247ed
Yuuraa <yoorachoi8937@gmail.com>
2022-02-05 21:24:35 +0900

    Add image location metadata when loading: with segmentation mask

--- dataset/cubox.py
+++ dataset/cubox.py
@@ -1,6 +1,9 @@
 import os
+import re
 import numpy as np
 from .transforms import get_transform
+import torchvision.transforms as transforms
+
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
 
@@ -18,14 +21,15 @@ class CUBOXdataset(Dataset):
         self.imgs = []
         self.img_names = []
         self.labels = []
+        self.selected_sample = []
 
         for cls_id, cls in enumerate(self.classes):
             cls_path = os.path.join(self.dataset_path, cls)
-            for filename in os.listdir(cls_path):
-                # img = Image.open(os.path.join(cls_path, filename))
+            is_sample = True
+            for filename in sorted(os.listdir(cls_path)):
                 self.img_names.append(os.path.join(cls_path, filename))
-                # self.imgs.append(np.array(img))
-                # img.close()
+                self.selected_sample.append(is_sample)
+                if is_sample: is_sample = False
                 self.labels.append(cls_id)
 
         self.num_classes = len(self.classes)
@@ -34,18 +38,49 @@ class CUBOXdataset(Dataset):
         # return len(self.imgs)
         return len(self.img_names)
 
+    def calc_img_loc(self, img_name):
+        seg_name = re.sub('.jpeg', '.png', re.sub('.jpg', '.png', re.sub('images', 'seg_map', img_name)))
+        try:
+            seg_map = Image.open(seg_name)
+        except:
+            print(img_name, seg_name)
+            return None, None
+        
+        seg_map = transforms.CenterCrop(224)(seg_map)
+        seg_map = np.array(seg_map)
+        nz_cs, nz_rs = (seg_map).nonzero()
+
+        try:
+            area_range = ((min(nz_rs), min(nz_cs)), (max(nz_rs), max(nz_cs))) # (rs, cs), (re, ce)
+        except:
+            print(seg_name)
+            return seg_map, ((0, 0), (0, 0))
+
+        return seg_map, area_range
+
+
     def __getitem__(self, idx):
         item_dict = dict()
         # img = self.imgs[idx]
         img_name = self.img_names[idx]
+        img = Image.open(img_name)
+        img = np.array(img)
+
+        seg_mask, img_locs = self.calc_img_loc(img_name)
+
+        img_metas = {
+            'img': img,
+            'img_locs': img_locs,
+            'seg_mask': seg_mask,
+        }
+
         if self.transform is not None:
-            img = Image.open(img_name)
-            img = np.array(img)
-            img = self.transform(img)
+            img = self.transform(img_metas)
         item_dict['img'] = img
         label = self.labels[idx]
         item_dict['gt'] = label
         item_dict['img_name'] = img_name
+        item_dict['is_sample'] = self.selected_sample[idx]
         return item_dict
 
 
