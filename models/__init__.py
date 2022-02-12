680bf877b1954b1216d1c51b332e9d6c46789839
Yuuraa <yoorachoi8937@gmail.com>
2022-02-12 23:29:13 +0900

    EfficientNet-b4 model

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 from torchvision.models.resnet import resnet50
 from .Dummy import DummyNetwork
+from efficientnet_pytorch import EfficientNet
 
 
 def get_model(model_name, num_classes, pretrained=True):
@@ -9,6 +10,11 @@ def get_model(model_name, num_classes, pretrained=True):
         model = resnet50(pretrained=pretrained)
         num_ftrs = model.fc.in_features
         model.fc = nn.Linear(num_ftrs, num_classes)
+    elif model_name == 'efficientnetv4':
+        if pretrained:
+            model = EfficientNet.from_pretrained('efficientnet-b4', num_classes=num_classes)
+        else:
+            model = EfficientNet.from_name('efficientnet-b4', num_classes=num_classes)
     elif model_name == 'dummy':
         return DummyNetwork(num_classes=num_classes, pretrained=pretrained)
     else:
